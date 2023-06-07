3cfb11c7093170063b6b376ff27d7aa09df19e22
mojemeno <hornak.rene@gmail.com>
2023-06-07 22:22:15 +0200

    added functions and basic game is almost working

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-let firstCard = 10;
-let secondCard = 4;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 
+const messageText = document.querySelector("#message");
+const cardText = document.querySelector("#card");
+const summarizeText = document.querySelector("#summarize");
 const gameButton = document.querySelector("#gameButton");
+const cardButton = document.querySelector("#newCardButton");
 
 gameButton.onclick = startGame;
+cardButton.onclick = newCard;
+
+function randomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    } else if (randomNumber === 1) {
+        return 11;
+    } else {
+        return randomNumber;
+    }
+}
 
 function startGame() {
+    isAlive = true;
+    let firstCard = randomCard();
+    let secondCard = randomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
+}
+
+function renderGame() {
+    cardText.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardText.textContent += cards[i] + " ";                
+    }
+    summarizeText.textContent = "Sum: " + sum;
+
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if (sum === 21) {
@@ -19,5 +49,13 @@ function startGame() {
         message = "You are out of the game!";
         isAlive = false;
     }
-    console.log(message);
+    messageText.textContent = message;
+}
+
+function newCard() {
+    let card = randomCard();
+    sum += card;
+    cards.push(card);
+    console.log(cards);
+    renderGame();
 }
